a4c5c75af8799bb4be0745d007995a582f7e857d
sbabureddy <sbabureddy01@gmail.com>
2024-03-25 22:43:49 +0530

    feat(page.tsx): add hero section
    Added hero section and customized tailiwind screens config.

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,19 @@ const josefinSans = Josefin_Sans({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="container">
-      <section className="grid grid-flow-row sm:grid-flow-col auto-rows-max">
-        <Image
-          src="https://img1.wsimg.com/isteam/stock/1257/:/rs=w:900,m"
-          height={500}
-          width={500}
-          alt=""
-          className="w-full h-full"
-        />
-        <div className="bg-slate-950 py-8 w-full">
-          <div className="text-center text-white">
+      <section className="flex  sm:flex-col flex-row w-full">
+        <div className="w-full flex">
+          <Image
+            src="https://img1.wsimg.com/isteam/stock/1257/:/rs=w:900,m"
+            height={500}
+            width={500}
+            alt=""
+            className="w-full h-full"
+            priority={true}
+          />
+        </div>
+        <div className="bg-slate-950 flex py-8 w-full">
+          <div className="text-center text-white flex-auto">
             <h1 className="mt-4 text-4xl">TRANSFORM YOUR SPACE</h1>
             <h2 className={`${josefinSans.className} mt-4  text-xl`}>
               Bringing life to your home with our interior design services
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        sm: { max: "576px" },
+        // => @media (min-width: 640px) { ... }
+
+        md: { max: "768px" },
+        // => @media (min-width: 768px) { ... }
+
+        lg: { max: "1024px" },
+        // => @media (min-width: 1024px) { ... }
+
+        xl: { max: "1280px" },
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": { max: "1536px" },
+        // => @media (min-width: 1536px) { ... }
+      },
     },
   },
   plugins: [],
